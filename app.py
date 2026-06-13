1fc174b656d70e2c3975c796d25fcccf6a8feb07
Shivani Kumari <shivaniku643@gmail.com>
2026-06-13 09:18:45 +0530

    Fix snake movement and logic

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # File ka full path batate hain
-    file_path = os.path.join(os.path.dirname(__file__), 'templates', 'snake.html')
-    with open(file_path, 'r') as f:
-        return f.read()
+    return render_template('snake.html')
 
 if __name__ == '__main__':
     app.run()
