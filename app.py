97bd71e6cd49870534e54a9f3ac045c11642ae6f
Shivani Kumari <shivaniku643@gmail.com>
2026-06-13 09:15:33 +0530

    Update app.py with full path

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('snake.html')
+    # File ka full path batate hain
+    file_path = os.path.join(os.path.dirname(__file__), 'templates', 'snake.html')
+    with open(file_path, 'r') as f:
+        return f.read()
 
 if __name__ == '__main__':
     app.run()
